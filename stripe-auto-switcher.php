def4200520ef6120948a9f420465355981b770e3
TruongTuyen <hieudieuluong@gmail.com>
2019-07-30 11:30:52 +0700

    Update cronjob time

--- stripe-auto-switcher.php
+++ stripe-auto-switcher.php
@@ -23,18 +23,37 @@ class Stripe_Auto_Switcher {
 		add_action( 'cmb2_admin_init', array( $this, 'render_options_page' ) );
 		add_action( 'wp_loaded', array( $this, 'register_woo_events' ) );
 		add_action( 'saw_event_do_switch', array( $this, 'do_schedule_action' ) );
+
+		add_action( 'update_option_saw-settings', array( $this, 'when_option_updated' ), 10, 3 );
+	}
+
+	public function when_option_updated( $old_value, $value, $option ) {
+		if ( isset( $old_value['recurrence_time'] ) && isset( $value['recurrence_time'] ) && $old_value['recurrence_time'] != $value['recurrence_time'] ) {
+			WC()->queue()->cancel_all( 'saw_event_do_switch', array(), 'saw_events' );
+		}
 	}
 
 	public function do_schedule_action() {
 		$this->update_stripe_settings();
 	}
 
+	public function get_setting_recurrence_time() {
+		$options      = get_option( 'saw-settings', array() );
+		$setting_time = 6;
+		if ( is_array( $options ) && isset( $options['recurrence_time'] ) && is_numeric( $options['recurrence_time'] ) ) {
+			$setting_time = absint( $options['recurrence_time'] );
+		}
+		return $setting_time;
+	}
+
 	public function register_woo_events() {
-		$events = array(
+		$options      = get_option( 'saw-settings', array() );
+		$setting_time = $this->get_setting_recurrence_time();
+		$events       = array(
 			'saw_event_do_switch' => array(
-				'args' => array(),
+				'args'  => array(),
 				'group' => 'saw_events',
-				'time' => 6 * 60 * 60,
+				'time'  => $setting_time * 60 * 60,
 			),
 		);
 
@@ -42,9 +61,9 @@ class Stripe_Auto_Switcher {
 			$args = wp_parse_args(
 				$args,
 				array(
-					'args' => array(),
+					'args'  => array(),
 					'group' => 'saw_events',
-					'time' => 6 * 60 * 60,
+					'time'  => $setting_time * 60 * 60,
 				)
 			);
 
@@ -56,21 +75,6 @@ class Stripe_Auto_Switcher {
 		}
 	}
 
-	public function register_cron_schedule( $schedules ) {
-		$schedules['every_6_hours'] = array(
-			'interval' => 6 * 60 * 60,
-			'display'  => esc_html__( 'Every 6 hours', 'saw' ),
-		);
-
-		return $schedules;
-	}
-
-	public static function register_schedule_action() {
-		if ( ! wp_next_scheduled( 'saw_run_switch' ) ) {
-			wp_schedule_event( time(), 'every_6_hours', 'saw_run_switch' );
-		}
-	}
-
 	public function update_stripe_settings() {
 		$options = get_option( 'saw-settings', array() );
 		if ( is_array( $options ) && isset( $options['stripe_infos'] ) && count( $options['stripe_infos'] ) > 1 ) {
@@ -78,9 +82,9 @@ class Stripe_Auto_Switcher {
 			reset( $stripe_infos );
 			$stripe_settings = get_option( 'woocommerce_stripe_settings', array() );
 
-			$current_publishable_key = $stripe_settings['publishable_key'];
-			$current_secret_key = $stripe_settings['secret_key'];
-			$get_index = -1;
+			$current_publishable_key = ( isset( $stripe_settings['publishable_key'] ) ) ? $stripe_settings['publishable_key'] : '';
+			$current_secret_key      = ( isset( $stripe_settings['secret_key'] ) ) ? $stripe_settings['secret_key'] : '';
+			$get_index               = -1;
 			foreach ( array_keys( $stripe_infos ) as $index => $key ) {
 				$item = $stripe_infos[ $key ];
 				if ( isset( $item['live_publishable_key'] ) && $item['live_publishable_key'] == $current_publishable_key && isset( $item['live_secret_key'] ) && $item['live_secret_key'] == $current_secret_key ) {
@@ -93,15 +97,18 @@ class Stripe_Auto_Switcher {
 			}
 			if ( isset( $info_data['live_publishable_key'] ) && ! empty( $info_data['live_publishable_key'] ) && isset( $info_data['live_secret_key'] ) && ! empty( $info_data['live_secret_key'] ) ) {
 				$stripe_settings['publishable_key'] = $info_data['live_publishable_key'];
-				$stripe_settings['secret_key'] = $info_data['live_secret_key'];
-				update_option( 'saw_current_val', $info_data['live_publishable_key'] );
+				$stripe_settings['secret_key']      = $info_data['live_secret_key'];
+				update_option( 'saw_current_publishable_key_val', $info_data['live_publishable_key'] );
+				update_option( 'saw_current_secret_key_val', $info_data['live_secret_key'] );
 				update_option( 'woocommerce_stripe_settings', $stripe_settings );
 			}
 		}
 	}
 
+
+
 	public function render_options_page() {
-		$cmb_options = new_cmb2_box(
+		$cmb_options     = new_cmb2_box(
 			array(
 				'id'           => 'saw-settings',
 				'title'        => esc_html__( 'Stripe Auto Switcher', 'teesight' ),
@@ -109,6 +116,54 @@ class Stripe_Auto_Switcher {
 				'option_key'   => 'saw-settings',
 				'menu_title'   => esc_html__( 'Stripe Switcher', 'teesight' ),
 				'save_button'  => esc_html__( 'Save Changes', 'teesight' ),
+			)
+		);
+		$stripe_settings = get_option( 'woocommerce_stripe_settings', array() );
+
+		$current_publishable_key = ( isset( $stripe_settings['publishable_key'] ) ) ? $stripe_settings['publishable_key'] : '';
+		$current_secret_key      = ( isset( $stripe_settings['secret_key'] ) ) ? $stripe_settings['secret_key'] : '';
+
+		$cmb_options->add_field(
+			array(
+				'name'       => esc_html__( 'Current publishable key', 'saw' ),
+				'id'         => '_saw_current_publishable_key',
+				'type'       => 'text',
+				'save_field' => false, // Otherwise CMB2 will end up removing the value.
+				'attributes' => array(
+					'readonly' => 'readonly',
+					'disabled' => 'disabled',
+				),
+				'default'    => $current_publishable_key,
+			)
+		);
+
+		$cmb_options->add_field(
+			array(
+				'name'       => esc_html__( 'Current secret key', 'saw' ),
+				'id'         => '_saw_current_secret_key',
+				'type'       => 'text',
+				'save_field' => false, // Otherwise CMB2 will end up removing the value.
+				'attributes' => array(
+					'readonly' => 'readonly',
+					'disabled' => 'disabled',
+				),
+				'default'    => $current_secret_key,
+			)
+		);
+
+		$recurrence_time_option = array();
+		for ( $i = 1; $i <= 24; $i++ ) {
+			$recurrence_time_option[ $i ] = sprintf( 'Every %d Hours', $i );
+		}
+		$cmb_options->add_field(
+			array(
+				'name'             => esc_html__( 'Recurrence Time', 'saw' ),
+				'desc'             => esc_html__( 'The hours do you want to switch account', 'saw' ),
+				'id'               => 'recurrence_time',
+				'type'             => 'select',
+				'show_option_none' => false,
+				'default'          => 6,
+				'options'          => $recurrence_time_option,
 			)
 		);
 
@@ -151,5 +206,17 @@ class Stripe_Auto_Switcher {
 
 new Stripe_Auto_Switcher();
 
-register_activation_hook( __FILE__, 'Stripe_Auto_Switcher::register_schedule_action' );
 
+function saw_sanitize_int( $value, $field_args, $field ) {
+	if ( ! is_numeric( $value ) ) {
+		$default = 0;
+		if ( isset( $field_args['default'] ) && is_numeric( $field_args['default'] ) ) {
+			$default = absint( $field_args['default'] );
+		}
+		$sanitized_value = $default;
+	} else {
+		// Ok, let's clean it up.
+		$sanitized_value = absint( $value );
+	}
+	return $sanitized_value;
+}
